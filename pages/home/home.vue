9cbf33d2404cfde1da3ed657426bb3a1dbbf5ac2
LuciaXu <3012170145@qq.com>
2022-10-31 19:13:40 +0800

    完成了 home 首页的开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,47 @@
 <template>
   <view>
-    
+    <!-- 轮播图 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="item in swiperList" :key="item.goods_id">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src" mode=""></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+
+    <!-- 分类导航区域 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </view>
+
+    <!-- 楼层区域 -->
+    <view class="floor-container">
+      <view class="floor-list" v-for="item in floorList">
+        <!-- 标题区域 -->
+        <view class="floor-title">
+          <image :src="item.floor_title.image_src" mode=""></image>
+        </view>
+        <!-- 盒子 -->
+        <view class="floor-box">
+          <!-- 左侧大图 -->
+          <navigator class="floor-left-img" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}"
+              mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧四个图 -->
+          <view class="floor-right-img">
+            <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0"
+              :url="item2.url">
+              <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
+
+
   </view>
 </template>
 
@@ -8,15 +49,111 @@
   export default {
     data() {
       return {
-        
+        // 轮播图的数据列表，默认为空数组
+        swiperList: [],
+        // 分类导航的数据列表
+        navList: [],
+        // 楼层的数据列表
+        floorList: [],
       }
     },
+    onLoad() {
+      this.getSwiperList()
+      this.getNavList()
+      this.getFloorList()
+    },
     methods: {
-      
+      // 获取轮播图
+      async getSwiperList() {
+        // 请求数据
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        // 获取失败
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 获取成功
+        this.swiperList = res.message
+      },
+
+      // 获取分类列表
+      async getNavList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/catitems')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.navList = res.message
+      },
+
+      // 导航图标点击跳转
+      navClickHandler(item) {
+        // 判断点击的是哪个 nav
+        if (item.name === '分类') {
+          uni.switchTab({
+            url: '../cate/cate'
+          })
+        }
+      },
+
+      // 楼层列表获取
+      async getFloorList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/floordata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 通过双层 forEach 循环，处理 URL 地址
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+          })
+        })
+        this.floorList = res.message
+      },
+
     }
   }
 </script>
 
-<style>
+<style lang="scss">
+  swiper {
+    height: 330rpx;
 
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+
+  .floor-title {
+    height: 60rpx;
+    width: 100%;
+    display: flex;
+
+    image {
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  .floor-box {
+    display: flex;
+    padding-left: 10rpx;
+  }
+
+  .floor-right-img {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
 </style>
