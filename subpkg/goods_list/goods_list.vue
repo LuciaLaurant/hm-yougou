924b93cc8bcb4214c306ca951daed4023001ac83
LuciaXu <3012170145@qq.com>
2022-11-01 12:59:06 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,24 @@
 <template>
   <view>
-    
+    <view class="goods-list">
+      <block v-for="(goods, i) in goodsList" :key="i">
+        <view class="goods-item" @click="gotoDetail(goods.goods_id)">
+          <!-- 商品左侧图片区域 -->
+          <view class="goods-item-left">
+            <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+          </view>
+          <!-- 商品右侧信息区域 -->
+          <view class="goods-item-right">
+            <!-- 商品标题 -->
+            <view class="goods-name">{{goods.goods_name}}</view>
+            <view class="goods-info-box">
+              <!-- 商品价格 -->
+              <view class="goods-price">￥{{goods.goods_price}}</view>
+            </view>
+          </view>
+        </view>
+      </block>
+    </view>
   </view>
 </template>
 
@@ -8,15 +26,84 @@
   export default {
     data() {
       return {
-        
+        // 请求参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0,
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
       }
     },
+    onLoad(options) {
+      // 将页面参数转存到 this.queryObj 对象中
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      // 调用获取商品列表数据的方法
+      this.getGoodsList()
+    },
     methods: {
-      
+      // 获取商品列表
+      async getGoodsList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值
+        this.goodsList = res.message.goods
+        this.total = res.message.total
+      },
+
+      // 去详情页
+      gotoDetail(itemId) {
+        // console.log(itemId)
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + itemId
+        })
+      },
     }
   }
 </script>
 
-<style>
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
 
+    .goods-item-left {
+      margin-right: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+  }
 </style>
